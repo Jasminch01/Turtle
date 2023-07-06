b7d70e3de3600134a74a638c1927497bdee90c8d
jasmin <jasminchakma895@gmail.com>
2023-07-06 12:25:37 +0600

    learn turtle

---
+++ Python filds.py
@@ -0,0 +1,24 @@
+
+# #love with turtle
+from turtle import*
+color("red")
+begin_fill()
+left(50)
+forward(100)
+circle(40, 180)
+left(260)
+circle(40, 180)
+forward(100)
+end_fill()
+exitonclick()
+# draw a ractangle with turtle
+import turtle;
+turtle.shape("turtle")
+turtle.speed(1)
+turtle.color("blue")
+turtle.begin_fill()
+for i in range(8): #for loop
+    turtle.forward(200)
+    turtle.left(90)
+turtle.end_fill()
+turtle.done()
