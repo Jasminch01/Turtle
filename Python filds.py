785c98dac50c6443f72b93b83e4f3abd10dc5d19
jasmin <jasminchakma895@gmail.com>
2023-07-22 17:37:11 +0600

    add trutle wheel

--- Python filds.py
+++ Python filds.py
@@ -22,3 +22,18 @@ for i in range(8): #for loop
     turtle.left(90)
 turtle.end_fill()
 turtle.done()
+
+# Turtle wheel
+
+import turtle;
+
+turtle.color("red")
+turtle.speed(5)
+counter = 0
+while counter < 36:
+    for i in range (4):
+        turtle.forward(100)
+        turtle.right(90)
+    turtle.right(10)
+    counter += 1
+turtle.exitonclick()
